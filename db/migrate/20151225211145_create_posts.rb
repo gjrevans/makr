36289f2743db2b18ddd448be501affcd7d3364fa
Gordon <gjrevans@gmail.com>
2015-12-30 21:34:30 -0700

    Updated more styling for homepage.

--- db/migrate/20151225211145_create_posts.rb
+++ db/migrate/20151225211145_create_posts.rb
@@ -4,7 +4,7 @@ class CreatePosts < ActiveRecord::Migration
       t.string :title
       t.string :link
       t.text :description
-
+      
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151226011123) do
+ActiveRecord::Schema.define(version: 20151231023726) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
